d39e1f72e93c7bd5aae07af67a87311676f55e5a
靳玉亮 <leo@air.local>
2018-06-14 17:44:31 -0500

    use module pattern

--- module_and_closure/js/module.js
+++ module_and_closure/js/module.js
@@ -12,6 +12,7 @@ function rudy() { // called each time the timer goes off
     document.getElementById("output").innerHTML += "Rudy!";
 }
 
+//This is just a function, not a module at all.
 function makeTimer(name) {
     return function() {
         if(timernew === null) {
@@ -26,3 +27,24 @@ function makeTimer(name) {
 }
 
 rudyTimer = makeTimer("rudy");
+
+//This is a module
+var timerModule = (function () {
+   var timer = null;
+
+   function rudyByModule(name) {
+       document.getElementById("outputnew").innerHTML += name;
+   };
+
+   var outputName = function () {
+       if (timer === null) {
+           timer = setInterval(rudyByModule, 1000, "rudy");
+       } else {
+           clearInterval(timer);
+           timer = null;
+       }
+   };
+
+   return {"out": outputName}; //Couldn't use outputName(), or it would be executed directly
+
+})();
